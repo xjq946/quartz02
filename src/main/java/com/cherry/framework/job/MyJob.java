a75f0c84d37e7b3959b27cddf8c478ce87f09caf
xjq946 <46616436+xjq946@users.noreply.github.com>
2020-11-27 16:32:08 +0800

    Update MyJob.java
    update commit

--- src/main/java/com/cherry/framework/job/MyJob.java
+++ src/main/java/com/cherry/framework/job/MyJob.java
@@ -6,13 +6,7 @@ import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import java.time.LocalDateTime;
 
-/**
- * Copyright (c) 2019 KYE Company
- * 跨越速运集团有限公司版权所有
- *
- * @author xiejiqing
- * @create 2019/5/4 17:05
- */
+
 public class MyJob extends QuartzJobBean {
 
     @Override
