4d2ea65e4db2dab49f46fb9857257f2185b4eacf
xjq946 <46616436+xjq946@users.noreply.github.com>
2020-12-04 11:51:42 +0800

    Update MyCronJob.java

--- src/main/java/com/cherry/framework/job/MyCronJob.java
+++ src/main/java/com/cherry/framework/job/MyCronJob.java
@@ -7,13 +7,6 @@ import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import java.util.Date;
 
-/**
- * Copyright (c) 2019 KYE Company
- * 跨越速运集团有限公司版权所有
- *
- * @author xiejiqing
- * @create 2019/5/4 17:08
- */
 public class MyCronJob extends QuartzJobBean {
 
 
