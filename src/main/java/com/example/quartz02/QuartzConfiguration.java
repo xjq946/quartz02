eca4ac5e137f706a3a6be16104175bd212d5a860
xiejiqing <xiejiqing@ky-tech.com.cn>
2020-12-04 14:09:53 +0800

    更新代码

--- src/main/java/com/example/quartz02/QuartzConfiguration.java
+++ src/main/java/com/example/quartz02/QuartzConfiguration.java
@@ -6,13 +6,7 @@ import org.quartz.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * Copyright (c) 2019 KYE Company
- * 跨越速运集团有限公司版权所有
- *
- * @author xiejiqing
- * @create 2019/5/4 17:07
- */
+
 @Configuration
 public class QuartzConfiguration {
 
